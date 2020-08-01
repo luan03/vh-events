e4e6f78fe23f5ad7dfb3a58f317b11951fa4c11a
Luan Fagundes <luan.fagundes@pebmed.com.br>
2020-08-01 12:17:38 -0300

    layout - meetup section

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1,80 +1,132 @@
-// custom map to iterate over NodeList object
-NodeList.prototype._map = Array.prototype.map;
 
-// open Event modal
-document.querySelectorAll('[data-target]')._map( (button) => {
+window.VH = {}
 
-    button.addEventListener('click', () => {
+VH.Extensions = {
+    map: function () {
+        // custom map to iterate over NodeList object
+        NodeList.prototype._map = Array.prototype.map;
+    },
 
-        const modal = button.getAttribute("data-target")
-        document.querySelector(`[data-modal="${modal}"]`).classList.remove('hide')
-    });
-})
-
-// close modal events
-document.querySelectorAll('.close')._map( (button) => {
-
-    button.addEventListener('click', () => {
-        document.querySelectorAll('.modal')._map( (modal) => {
-            modal.classList.add('hide')
-            modal.setAttribute('data-status','none')
-         })
-    });
-})
-
-// join event
-document.querySelectorAll('[data-action]')._map( (button) => {
-
-    button.addEventListener('click', () => {
-        const modal = button.getAttribute("data-action")
-        document.querySelector(`[data-modal="${modal}"]`).setAttribute('data-status','message')
-    });
-})
-
-// apply event
-document.querySelectorAll('[data-apply]')._map( (button) => {
-
-    button.addEventListener('click', () => {
-
-        if (button.getAttribute("data-apply") === "true") {
-            if (button.textContent === "Applied") {
-                return
-            }
-
-            disableJoinEvent()
-
-            button.innerHTML = "Applied";
-            document.querySelector('.toast.success').classList.remove('hide')
-            hideToast()
-        } else {
-
-            document.querySelector('.toast.error').classList.remove('hide')
-            hideToast()
-        }
-    });
-})
-
-// disable button Join Event
-function disableJoinEvent() {
-    alert('disable button Join Event')
+    init: function () {
+        this.map()
+    }
 }
 
-// waiting list
-document.querySelector('[data-list]').addEventListener('click', () => {
-    document.querySelector('.info').classList.remove('hide')
-    hideToast()
-});
+VH.toast = {
 
-// hide toast
-function hideToast() {
-
-    const displayTime = 5000
-
-    setTimeout(()=> {
+    close: function () {
         document.querySelectorAll('.toast')._map( (toast) => {
-            toast.classList.add('hide')
+
+            toast.addEventListener('click', () => {
+                toast.classList.add('hide')
+            });
         })
-    }, displayTime)
+    },
+
+    hide: function () {
+        const displayTime = 3000
+
+        setTimeout(()=> {
+            document.querySelectorAll('.toast')._map( (toast) => {
+                toast.classList.add('hide')
+            })
+        }, displayTime)
+    },
+
+    error: function () {
+        document.querySelector('.toast.error').classList.remove('hide')
+        this.hide()
+    },
+
+    info: function () {
+        document.querySelector('[data-list]').addEventListener('click', () => {
+            document.querySelector('.toast.info').classList.remove('hide')
+            this.hide()
+        });
+    },
+
+    success: function () {
+        document.querySelector('.toast.success').classList.remove('hide')
+        this.hide()
+    },
+
+    init: function () {
+        this.close()
+        this.info()
+    }
 }
 
-// close toast
+VH.modal = {
+    open: function () {
+        document.querySelectorAll('[data-target]')._map( (button) => {
+
+            button.addEventListener('click', () => {
+
+                const modal = button.getAttribute("data-target")
+                document.querySelector(`[data-modal="${modal}"]`).classList.remove('hide')
+            });
+        })
+    },
+    close: function () {
+        document.querySelectorAll('.close')._map( (button) => {
+
+            button.addEventListener('click', () => {
+                document.querySelectorAll('.modal')._map( (modal) => {
+                    modal.classList.add('hide')
+                    modal.setAttribute('data-status','none')
+                 })
+            });
+        })
+    },
+    init: function () {
+        this.open()
+        this.close()
+    }
+}
+
+VH.events = {
+    join: function () {
+        document.querySelectorAll('[data-action]')._map( (button) => {
+
+            button.addEventListener('click', () => {
+                const modal = button.getAttribute("data-action")
+                document.querySelector(`[data-modal="${modal}"]`).setAttribute('data-status','message')
+            });
+        })
+    },
+
+    apply: function () {
+        document.querySelectorAll('[data-apply]')._map( (button) => {
+
+            button.addEventListener('click', () => {
+
+                if (button.getAttribute("data-apply") === "true") {
+                    if (button.textContent === "Applied") {
+                        return
+                    }
+
+                    // disable button Join Event [CONTINUE]
+
+
+                    // success
+                    button.innerHTML = "Applied";
+                    VH.toast.success()
+
+                } else {
+                    VH.toast.error()
+                }
+            });
+        })
+    },
+
+    init: function () {
+        this.join()
+        this.apply()
+    }
+}
+
+VH.Extensions.init()
+
+VH.modal.init()
+VH.toast.init()
+VH.events.init()
